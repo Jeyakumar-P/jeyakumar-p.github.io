73dc04a2768e7240007173a8130ca913c954fe9c
JEYAKUMAR P <71009917+Jeyakumar-P@users.noreply.github.com>
2023-05-07 12:51:43 +0530

    New Changes 02

--- projects/js/main.js
+++ projects/js/main.js
@@ -172,3 +172,18 @@
     });
 
 })(jQuery);
+
+var btn = $('#scrolltotop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
